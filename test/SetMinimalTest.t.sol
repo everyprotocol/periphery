2256aefac4beef2bf0b508d80c6f9fd7fc2cebd0
Brice <briceyan3.0@gmail.com>
2025-05-03 20:00:15 +0800

    refactor: rename testElements to elems in SetMinimalTest

--- test/SetMinimalTest.t.sol
+++ test/SetMinimalTest.t.sol
@@ -7,7 +7,7 @@ contract SetMinimalTest is Test {
     MySetMinimal set;
     address owner = makeAddr("owner");
     address user = makeAddr("user");
-    bytes32[] testElements = [bytes32("elem1"), bytes32("elem2")];
+    bytes32[] elems = [bytes32("elem1"), bytes32("elem2")];
 
     function setUp() public {
         set = new MySetMinimal(17, 1, 18, 2);
@@ -15,7 +15,7 @@ contract SetMinimalTest is Test {
 
     function test_Mint() public {
         vm.prank(user);
-        (uint64 id, Descriptor memory desc) = set.mint(user, testElements);
+        (uint64 id, Descriptor memory desc) = set.mint(user, elems);
 
         assertEq(id, 1, "First mint should have ID 1");
         assertEq(desc.kindId, set._kindId(), "Kind ID should match");
@@ -24,28 +24,8 @@ contract SetMinimalTest is Test {
         assertEq(desc.setRev, set._setRev(), "Set revision should match");
     }
 
-    function test_UnsupportedKindId() public {
-        vm.expectRevert(abi.encodeWithSelector(MySetMinimal.UnsupportedKindId.selector));
-        set._kindRevision(999, 0);
-    }
-
-    function test_UnsupportedKindRevision() public {
-        vm.expectRevert(abi.encodeWithSelector(MySetMinimal.UnsupportedKindRevision.selector));
-        set._kindRevision(set._kindId(), set._kindRev() + 1);
-    }
-
-    function test_UnsupportedSetId() public {
-        vm.expectRevert(abi.encodeWithSelector(MySetMinimal.UnsupportedSetId.selector));
-        set._setRevision(999, 0);
-    }
-
-    function test_UnsupportedSetRevision() public {
-        vm.expectRevert(abi.encodeWithSelector(MySetMinimal.UnsupportedSetRevision.selector));
-        set._setRevision(set._setId(), set._setRev() + 1);
-    }
-
     function test_Uri() public {
-        string memory uri = set._uri();
+        string memory uri = set.uri();
         assertEq(uri, "http://image.local/mysetminimal/{id}/{rev}/meta", "URI should match");
     }
 }
